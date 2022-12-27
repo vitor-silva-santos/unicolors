986698a7938db7cb010298c1b903bb1c712c1407
vitor-silva-santos <vithorss2203@gmail.com>
2022-12-27 19:59:47 -0300

    Atualizando cores e criando seção produtos

--- assets/JS/navBar.js
+++ assets/JS/navBar.js
@@ -1,6 +1,14 @@
 const checkMenu = document.getElementById("menu-burguer");
 const menu = document.querySelector(".menu-option");
+const main = document.querySelector(".main");
 
 checkMenu.addEventListener("click", () => {
   menu.classList.toggle("active");
+});
+
+main.addEventListener("click", () => {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    checkMenu.checked = false;
+  }
 });
